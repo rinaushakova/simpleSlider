d8099f4d3d709209ac016bcdc14351df9e007896
Ekaterina <rina_ushakova@bk.ru>
2021-06-15 12:29:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const slides = document.querySelectorAll('.slide');
+
+
+slides.forEach(slide => {
+    slide.addEventListener('click', () => {
+
+        clearActive()
+
+        slide.classList.add('active');
+    })
+});
+
+function clearActive() {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    })
+}
